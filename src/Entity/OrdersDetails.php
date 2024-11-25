3de2d785fdd2d92b9075b93dee22937a6cbe58f2
Guillaume-tllrd <guillaumetillard10@gmail.com>
2024-11-25 18:12:42 +0100

    db created

---
+++ src/Entity/OrdersDetails.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersDetailsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersDetailsRepository::class)]
+class OrdersDetails
+{
+    // On a enlever l'id qui a été généré automatique lors de la création de l'entity. cependant on a plus de clé primaire, donc on rajoute #[ORM\Id] à products et orders. donc les 2 sont clés primaire qui va généré une clé primaire composite
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'ordersDetails')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Orders $orders = null;
+
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'ordersDetails')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Products $products = null;
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getOrders(): ?Orders
+    {
+        return $this->orders;
+    }
+
+    public function setOrders(?Orders $orders): static
+    {
+        $this->orders = $orders;
+
+        return $this;
+    }
+
+    public function getProducts(): ?Products
+    {
+        return $this->products;
+    }
+
+    public function setProducts(?Products $products): static
+    {
+        $this->products = $products;
+
+        return $this;
+    }
+}
